3885e67088039dced56a283fc41f4eac1bca1479
shaporin.v@gmail.com <shaporin.v@gmail.com>
2023-02-16 14:25:11 +0200

    16.02.14:25

--- server.py
+++ server.py
@@ -24,13 +24,14 @@ vendors = [
     {'name': 'mikrotik', 'url': 'mikrotik'},
     {'name': 'intel', 'url': 'intel'}
 ]
-params = ['Last 20', 'week', 'month', 'year']
+params = ['week', 'month', 'year']
 
 #static variables for timelimits
 now = dt.datetime.today()
 week = now - dt.timedelta(days=7)
 month = now - dt.timedelta(days=30)
 year = now - dt.timedelta(days=365)
+t=[week, month, year]
 
 #app function
 app = Flask(__name__)
@@ -38,18 +39,29 @@ app = Flask(__name__)
 #selecting last 20 documents for vendors
 elem=[]
 li={'id':1, 'Published':1, 'vendors':1, 'last-modified':1,'_id':0}
-'''def last_20(*vendor):
+def last_20(vendor):
     elem.clear()
     for i in collection.find({'vendors': vendor}, li).sort('last-modified',-1).limit(20):
         elem.append(i)
     return elem
-'''
 
-def choice (**kwargs):
+def t_limit(vendor, chas):
+    elem.clear()
+    if chas=='week': c=0
+    elif chas=='month': c=1
+    elif chas=='year': c=2
+    for i in collection.find({'$and':[{'last-modified':{'$gt':t[c]}}, {'vendors': vendor}]}, li).sort('last-modified', -1):
+        elem.append(i)
+
 
 @app.route("/")
 def index():
     return render_template("index.html", vendors=vendors, params=params)
+
+@app.route("/<vendor>/<chas>")
+def tt_limit(vendor, chas):
+    t_limit(vendor, chas)
+    return render_template('vendor.html', vendors=vendors, params=params, e=elem, v=vendor)
 
 
 @app.route("/<vendor>")
