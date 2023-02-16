9d6030a0308afeea569da985bf3a599dfaa822e0
shaporin.v@gmail.com <shaporin.v@gmail.com>
2023-02-16 15:13:50 +0200

    16.02/15.13

--- server.py
+++ server.py
@@ -39,9 +39,9 @@ app = Flask(__name__)
 #selecting last 20 documents for vendors
 elem=[]
 li={'id':1, 'Published':1, 'vendors':1, 'last-modified':1,'_id':0}
-def last_20(vendor):
+def last_15(vendor):
     elem.clear()
-    for i in collection.find({'vendors': vendor}, li).sort('last-modified',-1).limit(20):
+    for i in collection.find({'vendors': vendor}, li).sort('last-modified',-1).limit(15):
         elem.append(i)
     return elem
 
@@ -61,12 +61,12 @@ def index():
 @app.route("/<vendor>/<chas>")
 def tt_limit(vendor, chas):
     t_limit(vendor, chas)
-    return render_template('vendor.html', vendors=vendors, params=params, e=elem, v=vendor)
+    return render_template('vendor.html', vendors=vendors, params=params, e=elem, v=vendor, c=chas)
 
 
 @app.route("/<vendor>")
 def vndr(vendor):
-    last_20(vendor)
+    last_15(vendor)
     return render_template("vendor.html", vendors=vendors, e=elem, v=vendor)
 
 if __name__=="__main__":
